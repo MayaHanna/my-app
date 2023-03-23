f212ff56cc5b5a05684b9ffa9e2127f2b09a2f8e
Maya Hanna <mayshanna.mh@gmail.com>
2023-03-23 11:39:27 +0200

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
+import { LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
+import DatePicker from 'react-date-picker';
+
+interface Rate {
+  value: number;
+  date: string;
+}
 
 function App() {
+
+  const [rates, setRates] = useState<Rate[]>([]);
+  const [endDate, setEndDate] = useState<Date>();
+  const [startDate, setStartDate] = useState<Date>();
+
+  const enableFetching = endDate && startDate && startDate < endDate && (endDate.getTime() - startDate.getTime()) <= 1000 * 60 * 60 * 24 * 14;
+
+  async function fetchData(date: Date):Promise<Rate>{
+    const dateAsISO = date.toISOString().split("T")[0];
+    const rawData = await (await fetch(`https://openexchangerates.org/api/historical/${dateAsISO}.json?app_id=dc971481899445898f79d05116d52967`)).json();
+    const rate: Rate = {value: rawData.rates["ILS"], date: dateAsISO};
+    return rate;
+  }
+
+  useEffect(()=>{   
+    const currentDate = new Date();
+    const twoWeeksAgo = new Date();
+    twoWeeksAgo.setDate(currentDate.getDate() - 14);
+
+    setStartDate(twoWeeksAgo);
+    setEndDate(currentDate);
+  },[]);
+
+  function getDatesInRange():Date[] {
+    if(!startDate || !endDate) return [];
+
+    let result = [];
+
+    const date = new Date(startDate);
+    let i = 0;
+    while(date < endDate){
+      date.setDate(startDate.getDate() + i);
+      result.push(new Date(date));
+      i++;
+    }
+    return result;
+  }
+
+  useEffect(()=>{   
+    if(!enableFetching) return;
+    const allData = Promise.all(getDatesInRange().map(d => fetchData(d)));
+    allData.then((res) => setRates(res));
+  },[startDate, endDate]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='date-range'>
+        <DatePicker value={startDate} onChange={setStartDate} />
+        <h1> - </h1>
+        <DatePicker value={endDate} onChange={setEndDate} />
+      </div>
+      {enableFetching &&
+      <LineChart width={600} height={400} data={rates}>
+        <Line type="monotone" dataKey="value" stroke="#8884d8" />
+        <XAxis dataKey="date" />
+         <YAxis  domain={[Math.floor(Math.min(...rates.map(r=>r.value))), Math.ceil(Math.max(...rates.map(r=>r.value)))]}/>
+         <Tooltip />
+      </LineChart>}
     </div>
   );
 }
